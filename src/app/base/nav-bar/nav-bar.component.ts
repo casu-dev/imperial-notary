9a7d3252e6ed12da519adc857e13195c36fc4566
Alexander <alexander97krischer@gmail.com>
2023-05-06 20:34:51 +0200

    navbar

--- src/app/base/nav-bar/nav-bar.component.ts
+++ src/app/base/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+
+  @Output() select = new EventEmitter<string>();
 
 }
