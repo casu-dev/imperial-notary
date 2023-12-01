b97f095ef902587c7c1e26db074060432b546168
Casu <alexander.krischer@icloud.com>
2023-12-01 11:57:43 +0100

    todo import characters

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
       this.router.navigate(['char', 'battle']).then();
     }
     else {
-      this.router.navigate(['create']).then();
+      //this.router.navigate(['create']).then();
     }
   }
 
